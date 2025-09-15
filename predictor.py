d3b4d82b6d589231823b971bf9033392605f02b0
AlexMoralesDev <alex.morales8@upr.edu>
2025-09-15 09:23:53 -0400

    Data Cleanup, removed fields like League, since they are all LaLiga

--- predictor.py
+++ predictor.py
@@ -12,4 +12,18 @@ headers = {"X-Auth-Token": API_KEY}
 url = "https://api.football-data.org/v4/competitions/2014/matches"
 response = requests.get(url, headers=headers)
 data = response.json()
-print(data['matches'])
+matches_list = []
+
+# Structure Data
+for match in data['matches']:
+    match_info = {'date' : match['utcDate'],
+                  'matchday' : match['matchday'],
+                  'home_team' : match['homeTeam']['name'],
+                  'home_team_id' : match['homeTeam']['id'],
+                  'away_team' : match['awayTeam']['name'],
+                  'away_team_id' : match['awayTeam']['id'],
+                  'home_score' : match['score']['fullTime']['home'],
+                  'away_score' : match['score']['fullTime']['away'],
+                  'winner' : match['score']['winner']}
+    matches_list.append(match_info)
+df = pd.DataFrame(matches_list)
